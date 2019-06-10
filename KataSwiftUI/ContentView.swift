88024556fa2d669526a432bc30d90f496e97a585
Davide Mendolia <davide@gokarumi.com>
2019-06-10 16:18:41 +0200

    Add KataSwiftUI target

---
+++ KataSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  KataSwiftUI
+//
+//  Created by Davide Mendolia on 10/06/2019.
+//  Copyright © 2019 Karumi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
